42d7451196d80ac7bd86c933a0123ebf6902d8ac
yangxl <yangxl@startimes.com.cn>
2018-05-22 15:36:15 +0800

    修改输出路径

--- kindergarten-portal/.umirc.js
+++ kindergarten-portal/.umirc.js
@@ -1,5 +1,6 @@
 export default {
   plugins: ['umi-plugin-dva'],
+  outputPath: './kindergarten',
   pages: {
     '/home': { Route: './src/routes/PrivateRoute.js' },
     '/video': { Route: './src/routes/PrivateRoute.js' },
