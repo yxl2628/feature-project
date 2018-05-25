0c2294cf8dc85962a85ad4ad42ea0c06a08a7970
yangxl <yangxl@startimes.com.cn>
2018-05-25 09:30:53 +0800

    临时文件

--- kindergarten-portal/src/pages/home/index.js
+++ kindergarten-portal/src/pages/home/index.js
@@ -4,7 +4,7 @@ import styles from './index.css'
 export default function(props) {
   return (
     <div className={styles.content}>
-      <div className={styles.title}>晋中市幼儿园分布图表</div>
+      <div className={styles.title}>某某市幼儿园分布图表</div>
       <div className={styles.subTitle}>（请选择进入详情）</div>
       <JinZhongMap />
     </div>
