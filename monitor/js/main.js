7d353ce9cb41de92ae1096391213ab8156a2ee87
yangxl <yangxl@startimes.com.cn>
2018-06-12 15:51:35 +0800

    首页增加横向带宽占比Top

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -216,7 +216,7 @@ $(document).ready(function() {
     })
     networkList = networkList.sort(function(a, b) {
       return b.value - a.value
-    }).slice(0, 5)
+    }).slice(0, 5).reverse()
     if (totalNetwork < 10000000) {
       totalNetwork = totalNetwork / 1000
       type = 'K'
@@ -240,42 +240,21 @@ $(document).ready(function() {
       color: ['#30af81', '#d1d41a', '#73b9bc', '#7289ab', '#91ca8c', '#f49f42'],
       grid: {
         top: 10,
-        left: 0,
+        left: 110,
         right: 0,
-        bottom: 70
+        bottom: 10
       },
-      xAxis: [{
+      yAxis: [{
         type: 'category',
         data: networkList.map(function(item) {
           return item.name
         }),
         axisLabel: {
-          formatter: function(value) {
-            value = value.split('_')[0]
-            var ret = ""; //拼接加\n返回的类目项
-            var maxLength = 1; //每项显示文字个数
-            var valLength = value.length; //X轴类目项的文字个数
-            var rowN = Math.ceil(valLength / maxLength); //类目项需要换行的行数
-            if (rowN > 1) //如果类目项的文字大于3,
-            {
-              for (var i = 0; i < rowN; i++) {
-                var temp = ""; //每次截取的字符串
-                var start = i * maxLength; //开始截取的位置
-                var end = start + maxLength; //结束截取的位置
-                //这里也可以加一个是否是最后一行的判断，但是不加也没有影响，那就不加吧
-                temp = value.substring(start, end) + "\n";
-                ret += temp; //凭借最终的字符串
-              }
-              return ret;
-            } else {
-              return value;
-            }
-          },
           color: '#b9b9b9',
           fontSize: 12
         }
       }],
-      yAxis: [{
+      xAxis: [{
         type: 'value',
         splitLine: {
           show: false
