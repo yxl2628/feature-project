baa2dbbf82ef2ebe32e5717c0190e80521274ea1
yang.xiaolong <yxl2628@qq.com>
2018-08-21 23:46:49 +0800

    修改版面样式

--- infomation/src/components/Content/index.js
+++ infomation/src/components/Content/index.js
@@ -3,18 +3,23 @@ import styles from './index.css'
 
 function Content({item}) {
   return (
-    <div className={styles.content}>
-      <div className={styles.title}>{item.title}</div>
-      <div className={styles.footer}>
-        <div className={styles.left}>
-          {item.type ? <span className={styles[item.key]}>{item.type}</span> : ''}
+    <div className={styles.body}>
+      <div className={styles.content}>
+        <div className={styles.title}>{item.title}</div>
+        <div className={styles.footer}>
+          <div className={styles.left}>
+            {item.type ? <span className={styles[item.key]}>{item.type}</span> : ''}
+          </div>
+          <div className={styles.right}>
+            <span className={styles.source}>{item.source}</span>
+
+          </div>
         </div>
-        <div className={styles.right}>
-          <span className={styles.source}>{item.source}</span>
-          <span className={styles.read}><i className="iconfont icon-yuedu1"></i>{item.read}</span>
-          <span className={styles.zan}><i className="iconfont icon-zan1"></i>{item.zan}</span>
-          <span className={styles.share}><i className="iconfont icon-fenxiang1"></i></span>
-        </div>
+      </div>
+      <div className={styles.btngroup}>
+        <span className={styles.read}><i className="iconfont icon-yuedu1"></i>{item.read}</span>
+        <span className={styles.zan}><i className="iconfont icon-zan1"></i>{item.zan}</span>
+        <span className={styles.share}><i className="iconfont icon-fenxiang1"></i>8972</span>
       </div>
     </div>
   )
--- infomation/src/components/NewsList/index.js
+++ infomation/src/components/NewsList/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Content from '../Content'
+import styles from './index.css'
 
 function NewsList({newsList}) {
   return (
-    <div>
+    <div className={styles.list}>
       {
         newsList.map((item) => (<Content key={item.id} item={item}></Content>))
       }
