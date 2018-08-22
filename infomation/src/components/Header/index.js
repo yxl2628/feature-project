6c409aab0b8dc952a1d8ce402a736dcd8b606a31
yangxl <yangxl@startimes.com.cn>
2018-08-22 13:59:14 +0800

    修改头条logo

--- infomation/src/components/Header/index.js
+++ infomation/src/components/Header/index.js
@@ -4,8 +4,7 @@ import styles from './index.css'
 function Header (){
   return (
     <div className={styles.header}>
-      {/* <img src={require('../../assets/logo.png')} className={styles.logo} alt="xuan123.com"/>
-      <img src={require('../../assets/title-logo.png')} className={styles.title} alt="每天看一点最具价值的信息！"/> */}
+      <img src={require('../../assets/logo.jpg')} className={styles.logo} alt="xuan123.com"/>
       xuan123.com
     </div>
   )
