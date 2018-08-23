726367b93f1011eaf505ec95ee1b5f95040afcd6
yangxl <yangxl@startimes.com.cn>
2018-08-23 09:28:37 +0800

    修改显示文字

--- infomation/src/components/Menu/index.js
+++ infomation/src/components/Menu/index.js
@@ -24,7 +24,7 @@ function Menu ({dispatch, pageData}){
         }} style={pageData.currentKey === item.key ? {color : '#d43d3d'} : {}} key={item.key}>{item.name}</li>))}
       </ul>
       <div className={styles.show} onClick={show}>{
-        pageData.show ? <span>全部<i className="iconfont icon-open"></i></span> :
+        pageData.show ? <span>更多<i className="iconfont icon-open"></i></span> :
         <span>收起<i className="iconfont icon-shouqi_m"></i></span>
       }</div>
     </div>
