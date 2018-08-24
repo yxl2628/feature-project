9609cf877b2efb13833bba48f7f41caa028b1bc2
yangxl <yangxl@startimes.com.cn>
2018-08-24 09:21:59 +0800

    增加列表页

--- infomation/.webpackrc.js
+++ infomation/.webpackrc.js
@@ -1,5 +1,11 @@
 export default {
   'publicPath': '/static/',
+  'theme': {
+    'brand-primary': '#cc6666',
+    'brand-primary-tap': '#d43d3d',
+    'button-height-sm': '25px',
+    'button-font-size-sm': '11px'
+  },
   'copy': [
     {
       'from': 'src/static/',
--- infomation/src/pages/mobile/detail.js
+++ infomation/src/pages/mobile/detail.js
@@ -2,6 +2,7 @@ import Header from '../../components/Header'
 import styles from './index.css'
 import { connect } from 'dva'
 import NavLink from 'umi/navlink'
+import { Button } from 'antd-mobile'
 
 function NewsDetail({pageData: {detail, newsList}}) {
   return (
@@ -14,19 +15,31 @@ function NewsDetail({pageData: {detail, newsList}}) {
           <div className={styles.detail}>
             <div className={styles.navbar}><NavLink to='/mobile/'>首页</NavLink> / {detail.type}</div>
             <div className={styles.currentNews}>
-              {detail.title}
-            </div>
-            <div className={styles.btngroup}>
-              <span className={styles.read}><i className="iconfont icon-yuedu"></i>{detail.read}</span>
-              <span className={styles.zan}><i className="iconfont icon-zan1"></i>{detail.zan}</span>
+              <div className={styles.currentNewsTitle}>{detail.title}</div>
+              <div className={styles.btngroup}>
+                <span className={styles.read}><i className="iconfont icon-yuedu"></i>{detail.read}</span>
+                <span className={styles.zan}><i className="iconfont icon-zan1"></i>{detail.zan}</span>
+                <div className={styles.view}><Button type="primary" size="small" inline onClick={() => {
+                  window.location.href = detail.link
+                }}>点击查看</Button></div>
+              </div>
             </div>
             <div className={styles.otherNews}>
-              <div className={styles.otherTitle}>
-                极力推荐：
+              <div className={styles.otherHeader}>
+                <span className={styles.otherTitle}>极力推荐：</span>
               </div>
               <div className={styles.list}>
                 {newsList.map((item => (
-                  item.id !== detail.id ? <div key={item.id} className={styles.item}>{item.title}</div> : ''
+                  item.id !== detail.id ? <div key={item.id} className={styles.item}>
+                    <div className={styles.newsTitle}>{item.title}</div>
+                    <div className={styles.btngroup}>
+                      <span className={styles.read}><i className="iconfont icon-yuedu"></i>{detail.read}</span>
+                      <span className={styles.zan}><i className="iconfont icon-zan1"></i>{detail.zan}</span>
+                      <div className={styles.view}><Button type="primary" size="small" inline onClick={() => {
+                        window.location.href = detail.link
+                      }}>点击查看</Button></div>
+                    </div>
+                  </div> : ''
                 )))}
               </div>
             </div>
