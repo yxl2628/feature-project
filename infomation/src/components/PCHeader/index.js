c622ad6a5afe0b71cb5eb0ffd7d514ae44f176fc
yangxl <yangxl@startimes.com.cn>
2018-10-11 09:08:49 +0800

    修改样式

--- infomation/src/components/PCHeader/index.js
+++ infomation/src/components/PCHeader/index.js
@@ -5,8 +5,8 @@ function Header (){
   return (
     <div className={styles.header}>
       <div className={styles.center}>
-        <div className={styles.logo}><a href="/pc/?category=a" ><img src={require('../../assets/pc_logo.png')} alt="" /></a></div>
-        <div className={styles.title}><img src={require('../../assets/pc_title.png')} alt="" /></div>
+        <div className={styles.logo}><a href="/pc/?category=a" ><img src={require('../../assets/pc_logo.png')} width="234px" height="45px" alt="" /></a></div>
+        <div className={styles.title}><img src={require('../../assets/pc_title.png')} width="308px" height="30px" alt="" /></div>
       </div>
     </div>
   )
